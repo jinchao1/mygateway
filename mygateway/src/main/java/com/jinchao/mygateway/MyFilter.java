84d83cd0fec5b18cd6cdeae9901538b455f4b813
jinchao <13718956081@139.com>
2022-01-04 21:37:32 +0800

    2022年1月4号

---
+++ mygateway/src/main/java/com/jinchao/mygateway/MyFilter.java
@@ -0,0 +1,40 @@
+package com.jinchao.mygateway;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+/**
+ * @Auther: jinchao
+ * @Date: 2022/1/1 - 9:35
+ * @Description: com.jinchao.mygateway
+ * @version: 1.0
+ */
+@Component
+public class MyFilter implements Ordered,GlobalFilter {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+
+        ServerHttpRequest request = exchange.getRequest();
+        MultiValueMap<String, String> queryParams = request.getQueryParams();
+        List<String> list = queryParams.get("id");
+        if(null == list || list.size() == 0){
+            //非法请求
+            System.out.println("不要！");
+        }
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
--- mygateway/src/main/java/com/jinchao/mygateway/MygatewayApplication.java
+++ mygateway/src/main/java/com/jinchao/mygateway/MygatewayApplication.java
@@ -2,8 +2,10 @@ package com.jinchao.mygateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MygatewayApplication {
 
     public static void main(String[] args) {
