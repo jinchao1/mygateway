27265084d502c205dfbcc328063262dd83d4a58c
jinchao <13718956081@139.com>
2022-01-04 22:09:11 +0800

    2022年1月4号

--- mygateway/src/main/java/com/jinchao/mygateway/MyFilter.java
+++ mygateway/src/main/java/com/jinchao/mygateway/MyFilter.java
@@ -3,6 +3,8 @@ package com.jinchao.mygateway;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.util.MultiValueMap;
@@ -15,6 +17,7 @@ import java.util.List;
  * @Auther: jinchao
  * @Date: 2022/1/1 - 9:35
  * @Description: com.jinchao.mygateway
+ *    自定义filter(过滤器)
  * @version: 1.0
  */
 @Component
@@ -29,6 +32,13 @@ public class MyFilter implements Ordered,GlobalFilter {
         if(null == list || list.size() == 0){
             //非法请求
             System.out.println("不要！");
+
+//            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+//            return exchange.getResponse().setComplete();
+
+            DataBuffer dataBuffer = exchange.getResponse().bufferFactory().wrap("下课了！".getBytes());
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().writeWith(Mono.just(dataBuffer));
         }
         return chain.filter(exchange);
     }
